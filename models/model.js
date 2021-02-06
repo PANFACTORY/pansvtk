5dfffeddab731e1e8d1174afce7195d09f1c94cb
PANFACTORY <yutatanabe510@gmail.com>
2021-02-06 14:17:30 +0900

    簡易VTKローダーを改良

--- models/model.js
+++ models/model.js
@@ -1,5 +1,5 @@
 module.exports.loadModelFromVTK = (_str) => {
-    let model = { POINTS : [], CELLS : [] };
+    let model = { POINTS : [], CELLS : [], POINT_DATAS : [], CELL_DATAS : [] };
 
     const tokens = _str.split(/\t+\r\n|\s+\r\n|\r\n|\t+|\s+|\r|\n/);
     for (let i = 0; i < tokens.length; i++) {
@@ -7,29 +7,43 @@ module.exports.loadModelFromVTK = (_str) => {
             const pointsnum = parseInt(tokens[i + 1]);
             i += 3;
             for (let j = 0; j < pointsnum; j++) {
-                let point = { x : parseFloat(tokens[i]), y : parseFloat(tokens[i + 1]), z : parseFloat(tokens[i + 2]) };
-                model.POINTS.push(point);
-                i += 3;
+                model.POINTS.push({ x : parseFloat(tokens[i++]), y : parseFloat(tokens[i++]), z : parseFloat(tokens[i++]) });
             }
         } else if (tokens[i] == 'CELLS') {
             const cellsnum = parseInt(tokens[i + 1]);
             i += 3;
             for (let j = 0; j < cellsnum; j++) {
-                const pointnum = parseInt(tokens[i]);
+                const pointnum = parseInt(tokens[i++]);
                 let cell = { POINTS : [], TYPE : -1 };
-                for (let k = 1; k <= pointnum; k++) {
-                    cell.POINTS.push(parseInt(tokens[i + k]));
+                for (let k = 0; k < pointnum; k++) {
+                    cell.POINTS.push(parseInt(tokens[i++]));
                 }
                 model.CELLS.push(cell);
-                i += pointnum + 1;
             }
         } else if (tokens[i] == 'CELL_TYPES') {
             const cellsnum = parseInt(tokens[i + 1]);
             i += 2;
             for (let j = 0; j < cellsnum; j++) {
-                model.CELLS[j].TYPE = parseInt(tokens[i]);
-                i++;
+                model.CELLS[j].TYPE = parseInt(tokens[i++]);
             }
+        } else if (tokens[i] == 'POINT_DATA') {
+
+        } else if (tokens[i] == 'CELL_DATA') {
+            const cellsnum = parseInt(tokens[i + 1]);
+            let celldata = { TAG : tokens[i + 3], VALUES : [] };
+            
+            if (tokens[i + 2] == 'SCALARS') {
+                i += 7;
+                for (let j = 0; j < cellsnum; j++) {
+                    celldata.VALUES.push(parseFloat(tokens[i++]));
+                }
+            } else if (tokens[i + 2] == 'VECTORS') {
+                i += 5;
+            } else if (tokens[i + 2] == 'TENSORS') {
+                i += 5;
+            }
+
+            model.CELL_DATAS.push(celldata);
         }
     }
 
