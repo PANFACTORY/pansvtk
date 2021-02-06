0ee7acf6f0e88e0ac73b725685ab1f2e54758385
PANFACTORY <yutatanabe510@gmail.com>
2021-02-07 00:56:04 +0900

    svgを返す関数を分離

--- views/view.js
+++ views/view.js
@@ -1,22 +1,26 @@
-const getSvgType9 = (_x1, _y1, _x2, _y2, _x3, _y3, _x4, _y4, _fill, _stroke, _width) => {
-    return `<polygon fill="${_fill}" stroke="${_stroke}" stroke-width="${_width}" points="${_x1},${_y1} ${_x2},${_y2} ${_x3},${_y3} ${_x4},${_y4}" />`
-}
+const svgs = require('./svgs');
 
 module.exports.getWebviewContent = (_fileName, _model, _colorTag="SolidColor") => {
     let svg = "";
-    let scale = 5;
+    let scale = 500;
     let X0 = 100;
     let Y0 = 500;
     for (let cell of _model.CELLS) {
-        const p0 = _model.POINTS[cell.POINTS[0]];
-        const p1 = _model.POINTS[cell.POINTS[1]];
-        const p2 = _model.POINTS[cell.POINTS[2]];
-        const p3 = _model.POINTS[cell.POINTS[3]];
-
         let id = _model.CELLS.indexOf(cell);
         const fillcolor = _colorTag == "SolidColor" ? "lime" : `hsl(${240*(1 - _model.CELL_DATAS[_colorTag][id])}, 100%, 50%)`;
 
-        svg += getSvgType9(X0 + p0.x*scale, Y0 - p0.y*scale, X0 + p1.x*scale, Y0 - p1.y*scale, X0 + p2.x*scale, Y0 - p2.y*scale, X0 + p3.x*scale, Y0 - p3.y*scale, fillcolor, "black", 1);
+        if (cell.TYPE == 5) {
+            const p0 = _model.POINTS[cell.POINTS[0]];
+            const p1 = _model.POINTS[cell.POINTS[1]];
+            const p2 = _model.POINTS[cell.POINTS[2]];
+            svg += svgs.getSvgType5(X0 + p0.x*scale, Y0 - p0.y*scale, X0 + p1.x*scale, Y0 - p1.y*scale, X0 + p2.x*scale, Y0 - p2.y*scale, fillcolor, "black", 1);
+        } else if (cell.type == 9) {
+            const p0 = _model.POINTS[cell.POINTS[0]];
+            const p1 = _model.POINTS[cell.POINTS[1]];
+            const p2 = _model.POINTS[cell.POINTS[2]];
+            const p3 = _model.POINTS[cell.POINTS[3]];
+            svg += svgs.getSvgType9(X0 + p0.x*scale, Y0 - p0.y*scale, X0 + p1.x*scale, Y0 - p1.y*scale, X0 + p2.x*scale, Y0 - p2.y*scale, X0 + p3.x*scale, Y0 - p3.y*scale, fillcolor, "black", 1);
+        }
     }
 
     let celldataoptions = '<option value="SolidColor">SolidColor</option>';
