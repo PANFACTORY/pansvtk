a18f9a91865d327d53af8ca8d197e0dc610d0091
PANFACTORY <yutatanabe510@gmail.com>
2021-02-06 15:01:05 +0900

    ビューの描画部分でセルデータに基づいて色塗り

--- views/view.js
+++ views/view.js
@@ -2,21 +2,28 @@ const getSvgLine = (_x1, _y1, _x2, _y2) => {
     return `<line x1="${_x1}" y1="${_y1}" x2="${_x2}" y2="${_y2}" stroke="aqua" stroke-width="1" />`;
 }
 
-const getSvgRectangle = (_x1, _y1, _x2, _y2, _x3, _y3, _x4, _y4) => {
-    return getSvgLine(_x1, _y1, _x2, _y2) + getSvgLine(_x2, _y2, _x3, _y3) + getSvgLine(_x3, _y3, _x4, _y4) + getSvgLine(_x4, _y4, _x1, _y1);
+const getSvgType9 = (_x1, _y1, _x2, _y2, _x3, _y3, _x4, _y4, _fill, _stroke, _width) => {
+    return `<polygon fill="${_fill}" stroke="${_stroke}" stroke-width="${_width}" points="${_x1},${_y1} ${_x2},${_y2} ${_x3},${_y3} ${_x4},${_y4}" />`
 }
 
 module.exports.getWebviewContent = (_fileName, _model) => {
     let svg = "";
     let scale = 5;
     let X0 = 10;
-    let Y0 = 300;
+    let Y0 = 500;
     for (let cell of _model.CELLS) {
         const p0 = _model.POINTS[cell.POINTS[0]];
         const p1 = _model.POINTS[cell.POINTS[1]];
         const p2 = _model.POINTS[cell.POINTS[2]];
         const p3 = _model.POINTS[cell.POINTS[3]];
-        svg += getSvgRectangle(X0 + p0.x*scale, Y0 - p0.y*scale, X0 + p1.x*scale, Y0 - p1.y*scale, X0 + p2.x*scale, Y0 - p2.y*scale, X0 + p3.x*scale, Y0 - p3.y*scale);
+
+        let id = _model.CELLS.indexOf(cell);
+        const R0 = 256*_model.CELL_DATAS[0].VALUES[id];
+        const R = ("0" + R0.toString(16)).slice(-2);
+
+        const fillcolor = `#${R}0000`
+
+        svg += getSvgType9(X0 + p0.x*scale, Y0 - p0.y*scale, X0 + p1.x*scale, Y0 - p1.y*scale, X0 + p2.x*scale, Y0 - p2.y*scale, X0 + p3.x*scale, Y0 - p3.y*scale, fillcolor, "black", 1);
     }
 	return `<!DOCTYPE html>
 <html lang="en" style="width:100%;height:100%;">
