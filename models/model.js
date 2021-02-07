69dec21803b9bf19c87a5de00c276815d9062e6a
PANFACTORY <yutatanabe510@gmail.com>
2021-02-07 12:09:49 +0900

    簡易VTKローダーのバグを修正

--- models/model.js
+++ models/model.js
@@ -1,6 +1,8 @@
 module.exports.loadModelFromVTK = (_str) => {
     let model = { POINTS : [], CELLS : [], POINT_DATAS : {}, CELL_DATAS : {} };
     let gridtype = "UNSTRUCTURED_GRID";
+    let datamode = "";
+    let datasnum = 0;
 
     const tokens = _str.split(/\t+\r\n|\s+\r\n|\r\n|\t+|\s+|\r|\n/);
     for (let i = 0; i < tokens.length;) {
@@ -68,55 +70,67 @@ module.exports.loadModelFromVTK = (_str) => {
             for (let j = 0; j < cellsnum; j++) {
                 model.CELLS[j].TYPE = parseInt(tokens[i++]);
             }
-        } else if (tokens[i] == 'POINT_DATA') {
-            const pointsnum = parseInt(tokens[i + 1]);
-            const tag = tokens[i + 3];
-            model.POINT_DATAS[tag] = { TYPE : "", VALUES : [] };
-
-            if (tokens[i + 2] == 'SCALARS') {
+        } else if (tokens[i] == 'POINT_DATA' || tokens[i] == 'CELL_DATA') {
+            datamode = tokens[i];
+            datasnum = parseInt(tokens[i + 1]);
+            i += 2;
+        } else if (tokens[i] == 'SCALARS') {
+            const tag = tokens[i + 1];
+            i += 5;
+            if (datamode == "POINT_DATA") {
+                model.POINT_DATAS[tag] = { TYPE : "", VALUES : [] };
                 model.POINT_DATAS[tag].TYPE = "SCALARS";
-                i += 7;
-                for (let j = 0; j < pointsnum; j++) {
+                for (let j = 0; j < datasnum; j++) {
                     model.POINT_DATAS[tag].VALUES.push(parseFloat(tokens[i++]));
                 }
-            } else if (tokens[i + 2] == 'VECTORS') {
-                model.POINT_DATAS[tag].TYPE = "VECTORS";
-                i += 5;
-                for (let j = 0; j < pointsnum; j++) {
-                    model.POINT_DATAS[tag].VALUES.push({ x : parseFloat(tokens[i++]), y : parseFloat(tokens[i++]), z : parseFloat(tokens[i++]) });
+            } else if (datamode == "CELL_DATA") {
+                model.CELL_DATAS[tag] = { TYPE : "", VALUES : [] };
+                model.CELL_DATAS[tag].TYPE = "SCALARS";
+                for (let j = 0; j < datasnum; j++) {
+                    model.CELL_DATAS[tag].VALUES.push(parseFloat(tokens[i++]));
                 }
-            } else if (tokens[i + 2] == 'TENSORS') {
+            }
+        } else if (tokens[i] == 'VECTORS') {
+            const tag = tokens[i + 1];
+            i += 3;
+            if (datamode == "POINT_DATA") {
+                model.POINT_DATAS[tag] = { TYPE : "", VALUES : [] };
+                model.POINT_DATAS[tag].TYPE = "VECTORS";
+                for (let j = 0; j < datasnum; j++) {
+                    model.POINT_DATAS[tag].VALUES.push({ 
+                        x : parseFloat(tokens[i++]), 
+                        y : parseFloat(tokens[i++]), 
+                        z : parseFloat(tokens[i++]) 
+                    });
+                }
+            } else if (datamode == "CELL_DATA") {
+                model.CELL_DATAS[tag] = { TYPE : "", VALUES : [] };
+                model.CELL_DATAS[tag].TYPE = "VECTORS";
+                for (let j = 0; j < datasnum; j++) {
+                    model.CELL_DATAS[tag].VALUES.push({ 
+                        x : parseFloat(tokens[i++]), 
+                        y : parseFloat(tokens[i++]), 
+                        z : parseFloat(tokens[i++]) 
+                    });
+                }
+            }
+        } else if (tokens[i] == 'TENSORS') {
+            const tag = tokens[i + 1];
+            i += 3;
+            if (datamode == "POINT_DATA") {
+                model.POINT_DATAS[tag] = { TYPE : "", VALUES : [] };
                 model.POINT_DATAS[tag].TYPE = "TENSORS";
-                i += 5;
-                for (let j = 0; j < pointsnum; j++) {
+                for (let j = 0; j < datasnum; j++) {
                     model.POINT_DATAS[tag].VALUES.push({ 
                         xx : parseFloat(tokens[i++]), xy : parseFloat(tokens[i++]), xz : parseFloat(tokens[i++]),
                         yx : parseFloat(tokens[i++]), yy : parseFloat(tokens[i++]), yz : parseFloat(tokens[i++]),
                         zx : parseFloat(tokens[i++]), zy : parseFloat(tokens[i++]), zz : parseFloat(tokens[i++])
                     });
                 }
-            }
-        } else if (tokens[i] == 'CELL_DATA') {
-            const cellsnum = parseInt(tokens[i + 1]);
-            const tag = tokens[i + 3];
-            model.CELL_DATAS[tag] = { TYPE : "", VALUES : [] };
-            
-            if (tokens[i + 2] == 'SCALARS') {
-                model.CELL_DATAS[tag].TYPE = "SCALARS";
-                i += 7;
-                for (let j = 0; j < cellsnum; j++) {
-                    model.CELL_DATAS[tag].VALUES.push(parseFloat(tokens[i++]));
-                }
-            } else if (tokens[i + 2] == 'VECTORS') {
-                model.CELL_DATAS[tag].TYPE = "VECTORS";
-                i += 5;
-                for (let j = 0; j < cellsnum; j++) {
-                    model.CELL_DATAS[tag].VALUES.push({ x : parseFloat(tokens[i++]), y : parseFloat(tokens[i++]), z : parseFloat(tokens[i++]) });
-                }
-            } else if (tokens[i + 2] == 'TENSORS') {
+            } else if (datamode == "CELL_DATA") {
+                model.CELL_DATAS[tag] = { TYPE : "", VALUES : [] };
                 model.CELL_DATAS[tag].TYPE = "TENSORS";
-                i += 5;
-                for (let j = 0; j < cellsnum; j++) {
+                for (let j = 0; j < datasnum; j++) {
                     model.CELL_DATAS[tag].VALUES.push({ 
                         xx : parseFloat(tokens[i++]), xy : parseFloat(tokens[i++]), xz : parseFloat(tokens[i++]),
                         yx : parseFloat(tokens[i++]), yy : parseFloat(tokens[i++]), yz : parseFloat(tokens[i++]),
