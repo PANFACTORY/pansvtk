9c5f5b0c10eb568ee2f039a413c748f60c92dfb2
PANFACTORY <yutatanabe510@gmail.com>
2021-02-07 21:05:14 +0900

    Update view.js

--- views/view.js
+++ views/view.js
@@ -23,6 +23,11 @@ module.exports.getWebviewContent = (_fileName) => {
             });
 
             const $svgs = document.getElementById("svgs");
+            let text = document.createElementNS('http://www.w3.org/2000/svg', 'text');
+            text.setAttribute('x', 10);
+            text.setAttribute('y', 10);
+            text.textContent = "hello";
+            $svgs.appendChild(text);
 
             window.addEventListener('message', event => {
                 const message = event.data;
